47c6e56c6af44e638475d03c95c2b2c77ca8cc90
Aman Mirza <aman.mirza@snyder.tech>
2021-10-07 16:18:16 +0500

    feat: custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { UsdPkrPipe } from './pipes/usd-pkr.pipe';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsdPkrPipe,
     
 
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
 ,FormsModule  ],
   providers: [],
   bootstrap: [AppComponent]
