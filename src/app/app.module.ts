9eb028bd24bba978cbf216635f36701e2a54f1bd
amanymous <aman.mirza9@yahoo.com>
2021-10-08 00:32:21 +0500

    feat: directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RedElDirective } from './red-el.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedElDirective
   ],
   imports: [
     BrowserModule,
