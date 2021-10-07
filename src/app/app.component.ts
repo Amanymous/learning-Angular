61afe8a9daa41c2e86c43dc64352112d8418ba10
Aman Mirza <aman.mirza@snyder.tech>
2021-10-07 12:22:36 +0500

    feat: working on todoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'learning-angular';
-  userData:any={}
-  getData(value:NgForm)
-  {
-    console.warn(value);
-    this.userData=value
+  title = 'Angular Template Driven Form';
+  list:any[]=[]
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item})
+    console.log(this.list)
+    console.log(item)
+  }
+  removeTask(id:number){
+    console.log(id)
+    this.list = this.list.filter(item=>item.id!== id)
   }
 }
