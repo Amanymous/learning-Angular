e347e92777172897f8c24c218abd35901508ed55
Aman Mirza <aman.mirza@snyder.tech>
2021-10-08 12:58:22 +0500

    feat: dynamic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [{
@@ -15,6 +16,10 @@ const routes: Routes = [{
 {
   path:'home/:id',
   component:HomeComponent
+},
+{
+  path:'**',
+  component:PagenotfoundComponent
 }
 ];
 
