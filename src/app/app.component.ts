68809f7201d0e1a203e22c17a1cbb930b9711b13
amanymous <aman.mirza9@yahoo.com>
2021-12-19 12:31:33 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-
 export class AppComponent {
-  title = 'learning-angular';
-  
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
